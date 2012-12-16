6f012450c1f0ee70fdc453317e12da6df8e8835d
Ethan Chen <intervigil@gmail.com>
2012-12-15 17:53:37 -0800

    primoc: clean cm.mk

--- cm.mk
+++ cm.mk
@@ -1,14 +1,21 @@
-$(call inherit-product, device/htc/primoc/full_primoc.mk)
+# Specify phone tech before including full phone
+$(call inherit-product, vendor/cm/config/cdma.mk)
 
-PRODUCT_RELEASE_NAME := One_V
+# Release name
+PRODUCT_RELEASE_NAME := primoc
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/cdma.mk)
+# Inherit device configuration
+$(call inherit-product, device/htc/primoc/device_primoc.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_primoc BUILD_FINGERPRINT=virgin_mobile/htc_primoc/primou:4.1.1/JRO03L/330937:user/release-keys PRIVATE_BUILD_DESC="4.1.1 JRO03L 330937 release-keys"
-
-PRODUCT_NAME := cm_primoc
+# Device naming
 PRODUCT_DEVICE := primoc
+PRODUCT_NAME := cm_primoc
+PRODUCT_BRAND := htc
+PRODUCT_MODEL := One V
+PRODUCT_MANUFACTURER := HTC
+
+# Set build fingerprint / ID / Product Name etc.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_primoc BUILD_FINGERPRINT=virgin_mobile/htc_primoc/primoc:4.0.3/IML74K/63812.6:user/release-keys PRIVATE_BUILD_DESC="1.08.652.6 CL63812 release-keys" BUILD_NUMBER=63812
